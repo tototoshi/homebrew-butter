cb06051a66293ebabb471033f96c39ba6c42b9c3
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2014-03-28 02:17:16 +0900

    init

---
+++ Casks/butter.rb
@@ -0,0 +1,7 @@
+class Butter < Cask
+  url 'https://github.com/harukasan/butter/releases/download/v0.1.1/Butter_0.1.1.dmg'
+  homepage 'http://harukasan.jp'
+  version '0.1.1'
+  sha256 '09a754564671024b76c3de618166a5a56f19e8ec6231d031d0ef5afbbd4104fa'
+  link 'Butter.app'
+end
